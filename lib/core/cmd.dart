85960d70c0e0712482f5f96dc9325a49204c9781
Ahmed Sobhy <55zizo33@gmail.com>
2024-06-04 21:14:47 +0300

    ahmed

---
+++ lib/core/cmd.dart
@@ -0,0 +1,16 @@
+/*
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AhmedSobhey/movies_mapAmit63.git
+git push -u origin main
+
+
+
+
+
+
+
+
+ */
